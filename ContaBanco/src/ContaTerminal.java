1d0e1f7b84a47bb569bfedd7f052e47779f60ad7
Silvério Neto <netores1@gmail.com>
2024-04-30 16:27:52 -0300

    Primeiro Commit Projeto Conta Banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo = 1258.99;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Número da agência: ");
+        agencia = scanner.nextLine();
+        System.out.println("Número da conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco," +
+                " sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" " +
+                "já está disponível para saque");
     }
 }
